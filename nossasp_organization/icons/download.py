e368fd8e48bea4875c54b9d817ce18c33e769690
Capi Etheriel <barraponto@gmail.com>
2011-10-25 22:52:54 -0200

    updating downloads to new terms

--- nossasp_organization/icons/download.py
+++ nossasp_organization/icons/download.py
@@ -5,29 +5,37 @@ from urllib import urlretrieve, urlopen
 from itertools import product
 
 categories = [
-        {'category': 'Conselho Municipal', 'color': '12b012', 'code': '16'},
-        {'category': 'Outros'  , 'color': 'ff22d2', 'code': '17'},
-        {'category': 'Associação de bairro'  , 'color': 'de0800', 'code': '18'},
-        {'category': 'Associação profissional', 'color': '2880af', 'code': '19'},
-        {'category': 'Sindicatos', 'color': 'fe900f', 'code': '20'}
+        {'category': 'Conselhos Municipais', 'color': 'eedc41', 'tid': '16'},
+        {'category': 'Ongs', 'color': '2880af', 'tid': '17'},
+        {'category': 'Associação de Bairro'  , 'color': 'de0800', 'tid': '18'},
+        {'category': 'Sindicatos de Trabalhadores', 'color': 'bd4448', 'tid': '19'},
+        {'category': 'Sindicatos Empresariais', 'color': '90159f', 'tid': '20'},
+        {'category': 'Movimentos Sociais', 'color': '12b012', 'tid': '21'},
+        {'category': 'Organizações Virtuais', 'color': 'fe900f', 'tid': '22'},
         ]
 
 fields = [
-        {'type': 'Assistencia e Inclusão Social', 'code': '1', 'icon': 'communitycentre', 'generate': 'offices/city-services/community-center'},
-        {'type': 'Cidadania', 'code': '2', 'icon': 'group-2', 'generate': 'offices/group'},
-        {'type': 'Comunicação', 'code': '3', 'icon': 'audio', 'generate': 'media/audio'},
-        {'type': 'Consumo', 'code': '4', 'icon': 'bank', 'generate': 'offices/bank'},
-        {'type': 'Cultura', 'code': '5', 'icon': 'world', 'generate': 'tourism/place-to-see/world'},
-        {'type': 'Direitos Humanos', 'code': '6', 'icon': 'strike', 'generate': 'events/manifestation'},
-        {'type': 'Educação', 'code': '7', 'icon': 'university', 'generate': 'health-education/education/university'},
-        {'type': 'Esporte', 'code': '8', 'icon': 'soccerfield', 'generate': 'sports/ball-sports/soccer-football'},
-        {'type': 'Habitação', 'code': '9', 'icon': 'apartment-3', 'generate': 'friends-family/apartment'},
-        {'type': 'Meio Ambiente', 'code': '10', 'icon': 'riparianhabitat', 'generate': 'nature/natural-marvels/riparian-habitat'},
-        {'type': 'Redes', 'code': '11', 'icon': 'share', 'generate': 'media/share'},
-        {'type': 'Saúde', 'code': '12', 'icon': 'firstaid', 'generate': 'health/education/first-aid'},
-        {'type': 'Segurança', 'code': '13', 'icon': 'police', 'generate': 'offices/police'},
-        {'type': 'Trabalho e Renda', 'code': '14', 'icon': 'factory', 'generate': 'industry/factory'},
-        {'type': 'Transporte', 'code': '15', 'icon': 'bus', 'generate': 'transportation/road-transportation/bus'},
+        {'type': 'Assistencia Social', 'tid': '1', 'icon': 'communitycentre', 'generate': 'offices/city-services/community-center'},
+        {'type': 'Cidadania', 'tid': '2', 'icon': 'group-2', 'generate': 'offices/group'},
+        {'type': 'Comunicação', 'tid': '3', 'icon': 'audio', 'generate': 'media/audio'},
+        {'type': 'Consumo', 'tid': '4', 'icon': 'bank', 'generate': 'offices/bank'},
+        {'type': 'Cultura', 'tid': '5', 'icon': 'world', 'generate': 'tourism/place-to-see/world'},
+        {'type': 'Direitos Humanos', 'tid': '6', 'icon': 'strike', 'generate': 'events/manifestation'},
+        {'type': 'Educação', 'tid': '7', 'icon': 'university', 'generate': 'health-education/education/university'},
+        {'type': 'Esporte', 'tid': '8', 'icon': 'soccerfield', 'generate': 'sports/ball-sports/soccer-football'},
+        {'type': 'Habitação', 'tid': '9', 'icon': 'apartment-3', 'generate': 'friends-family/apartment'},
+        {'type': 'Meio Ambiente', 'tid': '10', 'icon': 'riparianhabitat', 'generate': 'nature/natural-marvels/riparian-habitat'},
+        {'type': 'Redes', 'tid': '11', 'icon': 'share', 'generate': 'media/share'},
+        {'type': 'Saúde', 'tid': '12', 'icon': 'firstaid', 'generate': 'health/education/first-aid'},
+        {'type': 'Segurança', 'tid': '13', 'icon': 'police', 'generate': 'offices/police'},
+        {'type': 'Trabalho e Renda', 'tid': '14', 'icon': 'factory', 'generate': 'industry/factory'},
+        {'type': 'Transporte', 'tid': '15', 'icon': 'bus', 'generate': 'transportation/road-transportation/bus'},
+        {'type': 'Raça e Etnia', 'tid': '25', 'icon': 'symbol_star', 'generate': 'numbers-letters'},
+        {'type': 'Gênero', 'tid': '26', 'icon': 'bus', 'generate': 'transportation/road-transportation/bus'},
+        {'type': 'Acessibilidade', 'tid': '27', 'icon': 'disability', 'generate': 'health-education/health/disability'},
+        {'type': 'Idosos', 'tid': '28', 'icon': 'seniorsite', 'generate': 'offices/senior-site'},
+        {'type': 'Criança e Adolescente', 'tid': '29', 'icon': 'toys', 'generate': 'stores/toys'},
+        {'type': 'Diversidade Sexual', 'tid': '30', 'icon': 'gay-male', 'generate': 'restaurants-bars/bars/gay-bar-male'},
         ]
 
 for combination in product(categories, fields):
@@ -35,4 +43,4 @@ for combination in product(categories, fields):
     print  'Getting ' + combination[1]['type'] + ' icon for ' + combination[0]['category'] + ' category.'
     urlopen('http://mapicons.nicolasmollet.com/markers/' + combination[1]['generate'] + '/?custom_color=' + combination[0]['color'])
     print  'Downloading from: ' + url
-    urlretrieve(url, combination[0]['code'] + '-' + combination[1]['code'] + '.png')
+    urlretrieve(url, combination[0]['tid'] + '-' + combination[1]['tid'] + '.png')
