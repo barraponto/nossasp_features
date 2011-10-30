9bad463e479865776387641fe08d45a6b5002b4a
Capi Etheriel <barraponto@gmail.com>
2011-10-30 14:08:38 -0200

    theming the thank and 404 pages

---
+++ nossasp_organization/thank-you.tpl.php
@@ -0,0 +1,7 @@
+<h1>Cadastro | <span class="and-title">Finalizado</h1>
+<div class="message">
+  <h2>Parabéns, o perfil da sua organização foi criado com sucesso!</h2>
+  <p>Em breve ela poderá ser visualizada no Mapa da Participação Cidadã.</p>
+  <p>Caso haja algum problema com o cadastro entraremos em contato.</p>
+  <p class="thank-you">Obrigado</p>
+</div>
