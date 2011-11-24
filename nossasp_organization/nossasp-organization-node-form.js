58d42a82954c3a7e76dbe9bd3791fa9b3d8bcf9e
Capi Etheriel <barraponto@gmail.com>
2011-11-23 22:32:50 -0200

    hide address for virtual orgs

--- nossasp_organization/nossasp-organization-node-form.js
+++ nossasp_organization/nossasp-organization-node-form.js
@@ -1,3 +1,19 @@
-Drupal.behaviors.NossaspOrganizationNodeForm = function (context) {
-  var $form = $('#node-form',context);
-}
+(function ($) {
+  Drupal.behaviors.NossaspOrganizationNodeForm = function (context) {
+    $('.cadastro #edit-primaryterm-wrapper + p', context).click(function(event){
+      $(this).next('.form-item').toggle('slow'); 
+      event.preventDefault();
+    });
+
+    var $addressfields = $('#edit-field-street-0-value-wrapper, #edit-field-street-0-value-wrapper', context);
+
+    $('#edit-field-tipo-value', context).change(function(){
+      if ($(this).val() == '22') {
+        $addressfields.hide('slow');
+      }
+      else {
+        $addressfields.show('slow');
+      }
+    }).change();
+  }
+} )(jQuery);
